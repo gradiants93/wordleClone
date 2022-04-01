04db58060226228b5b14ab492b1dab6f8a1deff2
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-04-01 12:14:55 -0700

    Checking to see if guess is same as mockword, no individual letter logic yet

--- client/src/components/gameboard.js
+++ client/src/components/gameboard.js
@@ -3,31 +3,50 @@ import Keyboard from "./keyboard";
 import { useState } from "react";
 
 export default function GameBoard() {
+  // states
+  //   const [word, setWord] = useState([]);
   const [col, setCol] = useState(1);
   const [row, setRow] = useState(1);
+  const [guess, setGuess] = useState([]);
+
+  // mock data
+  const mockWord = ["a", "p", "p", "l", "e"];
+  const [word, setWord] = useState(mockWord);
+  //handlers
   const onDelete = () => {
     if (col > 1) {
       setCol(col - 1);
+      guess.pop();
+      setGuess(guess);
+      document.getElementById(`(${row}, ${col - 1})`).innerHTML = " ";
+      document.getElementById(`(${row}, ${col - 1})`).letter = " ";
     }
   };
   const onCheck = () => {
     if (col === 6) {
       setCol(1);
       setRow(row + 1);
+      console.log("guess", guess, "word", word, guess.join() === word.join());
+      setGuess([]);
     }
   };
   const onChar = (letter) => {
     if (col < 6 && row < 7) {
       // row is 7 unnecessary?
       document.getElementById(`(${row}, ${col})`).innerHTML = letter;
+      document.getElementById(`(${row}, ${col})`).letter = letter;
       console.log(
         `new val for (${row}, ${col})`,
-        (document.getElementById(`(${row}, ${col})`).letter = letter)
+        document.getElementById(`(${row}, ${col})`).letter
       );
       setCol(col + 1);
+      setGuess(guess.concat(letter.toLowerCase()));
     }
     return letter;
   };
+
+  // logic
+
   return (
     <div>
       <GuessRow row={1} />
